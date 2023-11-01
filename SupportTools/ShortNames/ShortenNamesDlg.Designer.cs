b5ca4deba25cd9c8a1d9046a5a1ac42eb60b4251
Fede Azzato <fedeazzato@gmail.com>
2023-11-01 17:36:47 -0300

    Make dialog localizable

--- SupportTools/ShortNames/ShortenNamesDlg.Designer.cs
+++ SupportTools/ShortNames/ShortenNamesDlg.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ShortenNamesDlg));
             this.toolDescription = new System.Windows.Forms.Label();
             this.btnCancel = new System.Windows.Forms.Button();
             this.btnOK = new System.Windows.Forms.Button();
@@ -38,78 +39,48 @@
             // 
             // toolDescription
             // 
-            this.toolDescription.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.toolDescription.Location = new System.Drawing.Point(12, 9);
+            resources.ApplyResources(this.toolDescription, "toolDescription");
             this.toolDescription.Name = "toolDescription";
-            this.toolDescription.Size = new System.Drawing.Size(577, 124);
-            this.toolDescription.TabIndex = 0;
-            this.toolDescription.Text = "Description taken from Resources.ShortenNamesToolDescription";
             // 
             // btnCancel
             // 
-            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            resources.ApplyResources(this.btnCancel, "btnCancel");
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(514, 225);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(75, 23);
-            this.btnCancel.TabIndex = 1;
-            this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
             // 
             // btnOK
             // 
-            this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            resources.ApplyResources(this.btnOK, "btnOK");
             this.btnOK.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.btnOK.Location = new System.Drawing.Point(433, 225);
             this.btnOK.Name = "btnOK";
-            this.btnOK.Size = new System.Drawing.Size(75, 23);
-            this.btnOK.TabIndex = 2;
-            this.btnOK.Text = "OK";
             this.btnOK.UseVisualStyleBackColor = true;
             // 
             // chkShortenAttributes
             // 
-            this.chkShortenAttributes.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.chkShortenAttributes.AutoSize = true;
-            this.chkShortenAttributes.Location = new System.Drawing.Point(28, 146);
+            resources.ApplyResources(this.chkShortenAttributes, "chkShortenAttributes");
             this.chkShortenAttributes.Name = "chkShortenAttributes";
-            this.chkShortenAttributes.Size = new System.Drawing.Size(334, 17);
-            this.chkShortenAttributes.TabIndex = 3;
-            this.chkShortenAttributes.Text = "Description taken from Resources.ShortenNamesAttributesOption";
             this.chkShortenAttributes.UseVisualStyleBackColor = true;
             this.chkShortenAttributes.CheckedChanged += new System.EventHandler(this.chk_CheckedChanged);
             // 
             // chkShortenTables
             // 
-            this.chkShortenTables.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.chkShortenTables.AutoSize = true;
-            this.chkShortenTables.Location = new System.Drawing.Point(28, 169);
+            resources.ApplyResources(this.chkShortenTables, "chkShortenTables");
             this.chkShortenTables.Name = "chkShortenTables";
-            this.chkShortenTables.Size = new System.Drawing.Size(322, 17);
-            this.chkShortenTables.TabIndex = 3;
-            this.chkShortenTables.Text = "Description taken from Resources.ShortenNamesTablesOption";
             this.chkShortenTables.UseVisualStyleBackColor = true;
             this.chkShortenTables.CheckedChanged += new System.EventHandler(this.chk_CheckedChanged);
             // 
             // chkShortenObjects
             // 
-            this.chkShortenObjects.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.chkShortenObjects.AutoSize = true;
-            this.chkShortenObjects.Location = new System.Drawing.Point(28, 192);
+            resources.ApplyResources(this.chkShortenObjects, "chkShortenObjects");
             this.chkShortenObjects.Name = "chkShortenObjects";
-            this.chkShortenObjects.Size = new System.Drawing.Size(326, 17);
-            this.chkShortenObjects.TabIndex = 3;
-            this.chkShortenObjects.Text = "Description taken from Resources.ShortenNamesObjectsOption";
             this.chkShortenObjects.UseVisualStyleBackColor = true;
             this.chkShortenObjects.CheckedChanged += new System.EventHandler(this.chk_CheckedChanged);
             // 
             // ShortenNamesDlg
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(601, 260);
             this.Controls.Add(this.chkShortenObjects);
             this.Controls.Add(this.chkShortenTables);
             this.Controls.Add(this.chkShortenAttributes);
@@ -120,7 +91,6 @@
             this.MinimizeBox = false;
             this.Name = "ShortenNamesDlg";
             this.ShowIcon = false;
-            this.Text = "Shorten Names";
             this.ResumeLayout(false);
             this.PerformLayout();
 
